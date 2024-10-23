28c58d37ab9b4e6e46274cc4ff12808000256be1
denzildsoza <denzildsouza3661@gmail.com>
2024-10-23 14:58:36 +0530

    tower of anoi

--- towerofanoi.cc
+++ towerofanoi.cc
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
 #include<conio.h>
 
+using namespace std;
 
+void tower_of_anoi(int n,char from,char to, char aux){
+    if(n==0){
+        return;
+    }
+    tower_of_anoi(n-1,from,aux,to);
+    cout<<"Moving "<<n<<" rod "<<from<<" Moving to rod "<<to<<endl;
+    tower_of_anoi(n-1,aux,to,from);
+}
 
 int main(){
-
+    int n = 3;
+    tower_of_anoi(n,'A','C','B');
     return 0;
 }
